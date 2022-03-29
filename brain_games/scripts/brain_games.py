e670551e1110c92973dc27a82187f18745ed7bf4
vladislav moryakov <sat1sfy@vk.com>
2022-03-29 20:14:57 +0300

    add cli.py and fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,17 @@
+import sys
+sys.path.insert(1, '/home/sat/python-project-lvl1/brain_games')
+
+import cli
+
 def main():
     print("Welcome to the Brain Games!")
+    cli.welcome_user()
+
+
 
 if __name__ == '__main__':
     main()
+    
+
+
+
